1af0a7deca3776708228b4b62aac588b4a76152d
Yunus ÖZCAN <yunus.ozcan@lab2023.com>
2012-09-21 14:30:14 +0300

    #20 User's avatar field must be a string

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,5 @@
 class Team < ActiveRecord::Base
   attr_accessible :description, :facebook_username, :github_username, :mail, :name, :twitter_username, :avatar
-  has_attached_file :avatar
-  validates_presence_of :description, :facebook_username, :github_username, :mail, :name, :twitter_username
-  validates_attachment :avatar, :presence => true
+  validates_presence_of :description, :facebook_username, :github_username, :mail, :name, :twitter_username, :avatar
   validates :mail, :uniqueness => true
 end
