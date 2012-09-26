49eab60a88d25aa8b527f26bd1cef7de2109eb95
Yunus ÖZCAN <yunus.ozcan@lab2023.com>
2012-09-26 09:53:10 +0300

    #14	Set up validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ActiveRecord::Base
   attr_accessible :description, :facebook_username, :github_username, :mail, :name, :twitter_username, :avatar
-  validates_presence_of :description, :facebook_username, :github_username, :mail, :name, :twitter_username, :avatar
+  validates_presence_of :description, :facebook_username, :github_username, :mail, :name, :twitter_username
   validates :mail, :uniqueness => true
   has_attached_file :avatar
+  validates_attachment :avatar, :presence => true
 end
