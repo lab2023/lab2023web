d4838387b9a854b0a1920f30640c1ce0d9fd4cce
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2012-10-05 19:14:48 +0300

    #6 Continues to...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def index
     @projects = Project.limit(6).order("id DESC")
   end
+
+  def contact
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Lab2023web::Application.routes.draw do
 
   root :to => 'pages#index'
 
+  match "/works" => "projects#index", :as => :works_path
+  match "/team" => "teams#index", :as => :team_path
+  match "/contact" => "pages#contact", :as => :contact_path
+
   devise_for :admins, :controllers => { :sessions => "admins/sessions", :registrations =>  "admins/registrations"}
   namespace :admins do
     resources :dashboard
