b2dd04a11e9bf47ecdd301198d70ac5ab537eddf
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2012-11-16 12:31:52 +0200

    Hot fix...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Lab2023web::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( admins.css admins.js )
+  config.assets.precompile += %w( admins.css admins.js documentup.min.js jquery.validate.bootstrap.js jquery.validate.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
